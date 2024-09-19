d9247c894a345181d1c9263164b036c8a95d09fe
Fernando Dias <fernando.dias@nexcore.com.br>
2024-09-19 17:19:32 -0300

    organizando css com headwind

--- src/pages/TodasFeatureView.vue
+++ src/pages/TodasFeatureView.vue
@@ -98,7 +98,7 @@ const childMsg = ref('Não existe mensagem do componente filho')
             <div class="flex flex-col gap-6">
                 <div>
                     <h2>Event Listeners</h2>
-                    <button class="border px-6 py-2 bg-slate-300 rounded-full" @click="count++"> clieque: {{ count }}</button>
+                    <button class="px-6 py-2 border rounded-full bg-slate-300" @click="count++"> clieque: {{ count }}</button>
                 </div>
 
                 <div>
@@ -115,59 +115,59 @@ const childMsg = ref('Não existe mensagem do componente filho')
 
                 <div>
                     <h2>Conditional Rendering</h2>
-                    <button class="border px-6 py-2 bg-slate-300 rounded-full" @click="toggle">Alternar</button>
+                    <button class="px-6 py-2 border rounded-full bg-slate-300" @click="toggle">Alternar</button>
                     <h1 v-if="awesome">Opa !!</h1>
                     <h1 v-else>E ai 😢</h1>
                 </div>
 
-                <div class=" flex flex-col gap-4">
+                <div class="flex flex-col gap-4 ">
                     <h2>List Rendering </h2>
                     <form @submit.prevent="addTodo" class="flex gap-4">
-                        <input class="border p-2 rounded-lg" v-model="newTodo" required placeholder="new todo">
+                        <input class="p-2 border rounded-lg" v-model="newTodo" required placeholder="new todo">
                         <button>Add Todo</button>
                     </form>
                     <ul>
-                        <li v-for="todo in todos" :key="todo.id" class="flex gap-4 justify-between p-2">
+                        <li v-for="todo in todos" :key="todo.id" class="flex justify-between gap-4 p-2">
                             {{ todo.text }}
-                            <button class="px-2 py-1 border bg-slate-200 rounded-lg" @click="removeTodo(todo)">X</button>
+                            <button class="px-2 py-1 border rounded-lg bg-slate-200" @click="removeTodo(todo)">X</button>
                         </li>
                     </ul>
                 </div>
 
-                <div class=" flex flex-col gap-4">
+                <div class="flex flex-col gap-4 ">
                     <h2>Computed Property</h2>
                     <form @submit.prevent="addTodo2" class="flex gap-4">
-                        <input class="border p-2 rounded-lg" v-model="newTodo2" required placeholder="new todo">
+                        <input class="p-2 border rounded-lg" v-model="newTodo2" required placeholder="new todo">
                         <button>Add Todo</button>
                     </form>
                     <ul>
-                        <li v-for="todo in filteredTodos" :key="todo.id" class="flex gap-4 justify-between p-2">
+                        <li v-for="todo in filteredTodos" :key="todo.id" class="flex justify-between gap-4 p-2">
                             <input type="checkbox" v-model="todo.done">
                             <span :class="{ done: todo.done }">{{ todo.text }}</span>
-                            <button class="px-2 py-1 border bg-slate-200 rounded-lg" @click="removeTodo2(todo)">X</button>
+                            <button class="px-2 py-1 border rounded-lg bg-slate-200" @click="removeTodo2(todo)">X</button>
                         </li>
                     </ul>
                     <div>
-                        <button class="px-2 py-1 border bg-slate-200 rounded-lg" @click="hideCompleted = !hideCompleted">
+                        <button class="px-2 py-1 border rounded-lg bg-slate-200" @click="hideCompleted = !hideCompleted">
                             {{ hideCompleted ? 'Mostrar todos' : 'Esconder Completados' }}
                         </button>
                     </div>
                 </div>
 
-                <div class=" flex flex-col gap-4">
+                <div class="flex flex-col gap-4 ">
                     <h2>Lifecycle and Template Refs</h2>
                     <p ref="pElementRef"> Ola mundo</p>
                 </div>
 
-                <div class=" flex flex-col gap-4">
+                <div class="flex flex-col gap-4 ">
                     <h2>Watchers</h2>
                     <p>Todo id: {{ todoId }}</p>
-                    <button class="px-2 py-1 border bg-slate-200 rounded-lg" @click="todoId++" :disabled="!todoData">Próxima tarefa</button>
+                    <button class="px-2 py-1 border rounded-lg bg-slate-200" @click="todoId++" :disabled="!todoData">Próxima tarefa</button>
                     <p v-if="!todoData">Loading...</p>
                     <pre v-else>{{ todoData }}</pre>
                 </div>
 
-                <div class=" flex flex-col gap-4">
+                <div class="flex flex-col gap-4 ">
                     <ChildComponent @response="(msg) => childMsg = msg" />
                     <p>{{ childMsg }}</p>
                 </div>
